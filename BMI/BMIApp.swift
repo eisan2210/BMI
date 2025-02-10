a3966122591eeb6481ed619ae72f9a25510a9845
eisan2210 <eisan2210@gmail.com>
2025-02-10 19:47:34 +0900

    Initial Commit

---
+++ BMI/BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIApp.swift
+//  BMI
+//
+//  Created by satoeisuke on 2025/02/10.
+//
+
+import SwiftUI
+
+@main
+struct BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BMI
+//
+//  Created by satoeisuke on 2025/02/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
